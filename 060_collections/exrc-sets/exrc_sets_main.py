b07b8d7f6e86910b58b5c7639ff3b55b115dad31
Thomas Auinger <thomas@auinger.de>
2025-05-27 09:23:04 +0200

    Vorlage Übung Set

---
+++ 060_collections/exrc-sets/exrc_sets_main.py
@@ -0,0 +1,21 @@
+from typing import Set, List
+
+def clean_names(names:List[str]) -> List[str]:
+    return [] # todo
+
+
+names1 = ["tom", "Jürgen", "ANNA", "max"]
+names2 = ["Tom", "  max ", "anna", "peter"]
+
+cleaned_names1 = clean_names(names1)
+cleaned_names2 = clean_names(names2)
+
+unique_names = set() # todo
+print(unique_names)
+
+
+unique_names = set() # todo
+final_names: List[str] = [] # todo
+# todo: do sorting
+print(final_names)
+
