aa80ea64c86d7012e77998d4284937f3e0e1dd78
Thomas Auinger <thomas@auinger.de>
2025-05-27 13:54:53 +0200

    various

---
+++ 060_collections/converting_collections.py
@@ -0,0 +1,12 @@
+list_of_names = ["Tom", "Joe", "Karla"]
+set_of_names = set(list_of_names)
+
+set_of_numbers = {1, 2, 3}
+list_of_numbers = list(set_of_numbers)
+
+map_of_persons = {"Tom":100, "Karla": 128}
+set_of_keys = set(map_of_persons.keys())
+list_of_values = list(map_of_persons.values())
+
+print(list_of_values)
+
