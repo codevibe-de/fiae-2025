9d874efba1b550f2536e9f31c536f50b21ce13cc
Thomas Auinger <thomas@auinger.de>
2025-05-27 13:54:53 +0200

    various

---
+++ 060_collections/sets.py
@@ -0,0 +1,12 @@
+set_of_numbers = {1, 1, 1}
+set_of_numbers.remove(1)
+print(set_of_numbers)
+print(len(set_of_numbers))
+
+
+
+vowels = {'a', 'e', 'i', 'o', 'u'}
+letters = {'H', 'e', 'l', 'l', 'o'}
+print(letters - vowels)
+
+print("  xx  ".strip())
